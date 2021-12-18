c4ddb6ecbd58183fb60d8af477376373db34a1fe
ReemMahm0ud <reemhashad82@gmail.com>
2021-12-18 18:58:56 +0200

    create API controllers

---
+++ test/app/Http/Controllers/APIs/EmployeesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\APIs;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class EmployeesController extends Controller
+{
+    //
+
+    public function index ()
+    {
+        $employee = DB::table('employees')
+        ->join('salaries','employees.emp_no', '=','salaries.emp_no')
+        ->select('employees.emp_no','employees.first_name','salaries.salary')
+        //->where('employees.emp_no','=','salaries.emp_no')
+        ->selectRaw('employees.emp_no,employees.first_name, sum(salaries.salary) as sum')
+        ->groupBy('employees.emp_no')
+        ->orderBy('sum', 'DESC')->first();
+
+        //$max = $employee->max('sum')->get();
+
+        return response()->json($employee);
+    }
+}
